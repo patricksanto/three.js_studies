1c525c76e4fac1fc1efa9eaa9b99753477378766
Patrick Santos <patrick.sg.santos@gmail.com>
2023-05-23 14:24:31 -0700

    first sphere created on the scene

--- threeJs/main.js
+++ threeJs/main.js
@@ -4,7 +4,7 @@ import * as THREE from "three";
 const scene = new THREE.Scene();
 
 //Create a sphere
- const geometry = new THREE.SphereGeometry( 3, 64, 64 );
+ const geometry = new THREE.SphereGeometry( 3, 64, 64);
  const material = new THREE.MeshStandardMaterial({
       color: '#00ff83',
       roughness: 0.4,
@@ -12,8 +12,23 @@ const scene = new THREE.Scene();
  const mesh = new THREE.Mesh( geometry, material );
  scene.add( mesh );
 
+ //Sizes
+ const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  }
+
+ //Light
+  const light = new THREE.PointLight( '#ffffff ', 1 , 100);
+  const light2 = new THREE.PointLight( '#f6e ', 1 , 100);
+  light.position.set(30 , 30, 40);
+  light2.position.set(-40 , 40, -20);
+  scene.add( light2 );
+  scene.add( light );
+
  //Add the Camera
- const camera = new THREE.PerspectiveCamera( 50, 800, 600 );
+ const camera = new THREE.PerspectiveCamera( 50, 800/600 );
+ camera.position.z = 10 ;
  scene.add( camera );
 
  //Renderer
